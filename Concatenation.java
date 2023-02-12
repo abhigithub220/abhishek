75943d5f90bf63b47c8169fc9b0576e426bf8713
abhigithub220 <125180030+abhigithub220@users.noreply.github.com>
2023-02-12 13:44:53 +0530

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,21 @@
+public class concatenation {
+    public static void main(String args[]){
+        String firstName = "abhishekkne101";
+        String lastName = "gmail.com";
+        String fullName = firstName + "@" + lastName;
+        // String fullName = firstName + "@" + lastName;
+        // System.out.println(fullName);
+        
+        // print length
+        System.out.println(fullName.length());
+
+        // charAt
+        for(int i=0; i<fullName.length(); i++){
+            System.out.println(fullName.charAt(i));
+        }
+
+
+
+    }
+    
+}
